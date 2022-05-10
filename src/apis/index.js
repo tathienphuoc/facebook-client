00d1ed512c94c98bcb2616681e6d0587cf0af5c4
tathienphuoc <tathienphuoc@gmail.com>
2022-05-10 16:13:53 +0700

    Cros problem

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:3001/api/",
-  baseURL: "https://facebook-api.onrender.com",
+  baseURL: "https://facebook-api.onrender.com/api/",
   withCredentials: true,
 });
 
